562b93e261d915ea0e888729a3bc2a49dcb4862e
Abeermohsien <aboorakhalil993@gmail.com>
2024-04-04 13:30:04 -0700

    fabric

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,44 +1,28 @@
 #!/usr/bin/python3
-# fabfile.
-import os.path
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
+"""
+fabric file"""
 
-env.hosts = ["54.144.152.30", "35.174.176.12"]
+from fabric.api import put, run, env
+from os.path import exists
+env.hosts = ['52.91.122.57', '52.201.165.108']
 
 
 def do_deploy(archive_path):
-    """
-    archeive distrbuting to web server
-    """
-    if os.path.isfile(archive_path) is False:
+    """archeive distriput"""
+    if exists(archive_path) is False:
         return False
-    file = archive_path.split("/")[-1]
-    splited = file.split(".")[0]
-
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+    try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        fp = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(fp, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, fp, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(fp, no_ext))
+        run('rm -rf {}{}/web_static'.format(fp, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(fp, no_ext))
+        return True
+    except:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(splited)).failed is True:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(splited)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, splited)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(splited, splited)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(splited)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(splited)).failed is True:
-        return False
-    return True
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,44 +1,50 @@
 #!/usr/bin/python3
-# fibric file.
-import os.path
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
+"""
+fabric file """
 
-env.hosts = ["54.144.152.30", "35.174.176.12"]
+from fabric.api import env, local, put, run
+from datetime import datetime
+from os.path import exists, isdir
+env.hosts = ['52.91.122.57', '52.201.165.108']
+
+
+def do_pack():
+    """TGZ generate"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except:
+        return None
 
 
 def do_deploy(archive_path):
-    """
-    archeive distrbuting.
-    """
-    if os.path.isfile(archive_path) is False:
+    """archieve distribute"""
+    if exists(archive_path) is False:
         return False
-    file = archive_path.split("/")[-1]
-    splited = file.split(".")[0]
+    try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except:
+        return False
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+
+def deploy():
+    """distribute archeieve"""
+    archive_path = do_pack()
+    if archive_path is None:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(splited)).failed is True:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(splited)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, splited)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, splited)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(splited)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(splited)).failed is True:
-        return False
-    return True
+    return do_deploy(archive_path)
